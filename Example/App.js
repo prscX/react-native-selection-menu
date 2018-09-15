4b2b5ce8bce879c198343776ed98bdd0d5d0e766
Pranav Raj Singh Chauhan <pranav.chauhan@metricstream.com>
2018-09-15 14:24:28 +0530

    - Android: adding support for Formsheet

--- Example/App.js
+++ Example/App.js
@@ -12,37 +12,78 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  Button
 } from "react-native";
 
 import { RNSelectionMenu } from 'react-native-selection-menu'
 
 type Props = {};
 export default class App extends Component<Props> {
-  _onPress() {
+  _onPress(props) {
     RNSelectionMenu.Show({
-      values: ['One', 'Two', 'Three'],
-      selectedValues: ['One', 'Two'],
+      values: [
+        "One",
+        "Two",
+        "Three",
+        "One",
+        "Two",
+        "Three",
+        "One",
+        "Two",
+        "Three",
+        "One",
+        "Two",
+        "Three",
+        "One",
+        "Two",
+        "Three",
+        "One",
+        "Two",
+        "Three",
+        "One",
+        "Two",
+        "Three"
+      ],
+      selectedValues: ["One", "Two"],
       selectionType: 1,
       presentationType: 0,
       enableSearch: true,
-      searchPlaceholder: 'Search Value',
-      searchTintColor: '#FFFFFF'
-    })
+      searchPlaceholder: "Search Value",
+      searchTintColor: "#FFFFFF",
+      ...props
+    });
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
+    return <View style={styles.container}>
+        <Button
           onPress={() => {
-            this._onPress();
+            this._onPress({
+              presentationType: 0
+            });
           }}
-        >
-          <Text>Click</Text>
-        </TouchableOpacity>
-      </View>
-    );
+          title={"Actionsheet"}
+        />
+        <View style={{ height: 30 }}></View>
+        <Button
+          onPress={() => {
+            this._onPress({
+              presentationType: 1
+            });
+          }}
+          title={"Alert"}
+        />
+        <View style={{height: 30}}></View>
+        <Button
+          onPress={() => {
+            this._onPress({
+              presentationType: 2
+            });
+          }}
+          title={"Formsheet"}
+        />
+      </View>;
   }
 }
 
--- android/src/main/java/ui/selectionmenu/RNSelectionMenuModule.java
+++ android/src/main/java/ui/selectionmenu/RNSelectionMenuModule.java
@@ -2,6 +2,8 @@
 package ui.selectionmenu;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -17,6 +19,11 @@ import com.irozon.alertview.objects.AlertAction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+
+import ir.mirrajabi.searchdialog.SimpleSearchDialogCompat;
+import ir.mirrajabi.searchdialog.core.BaseSearchDialogCompat;
+import ir.mirrajabi.searchdialog.core.SearchResultListener;
+import ir.mirrajabi.searchdialog.core.Searchable;
 
 public class RNSelectionMenuModule extends ReactContextBaseJavaModule {
 
@@ -50,20 +57,59 @@ public class RNSelectionMenuModule extends ReactContextBaseJavaModule {
     String searchTintColor = props.getString("searchTintColor");
 
     AlertView alert = new AlertView(title, "Message", AlertStyle.BOTTOM_SHEET);
-    if (presentationType == 0) {
-      alert = new AlertView(title, "Message", AlertStyle.IOS);
-    } else if (presentationType == 1) {
-      alert = new AlertView(title, "Message", AlertStyle.DIALOG);
-    }
+    if (presentationType == 0 || presentationType == 1) {
+      if (presentationType == 0) {
+        alert = new AlertView(title, "Message", AlertStyle.IOS);
+      } else if (presentationType == 1) {
+        alert = new AlertView(title, "Message", AlertStyle.DIALOG);
+      }
 
-    for (int i = 0;i < values.size();i++) {
+      for (int i = 0;i < values.size();i++) {
         alert.addAction(new AlertAction(values.getString(i), AlertActionStyle.DEFAULT, new AlertActionListener() {
-        @Override
-        public void onActionClick(@NotNull AlertAction action) {
-        }
-      }));
+          @Override
+          public void onActionClick(@NotNull AlertAction action) {
+          }
+        }));
+      }
+
+      alert.show(getCurrentActivity());
+    } else if (presentationType == 2) {
+      ArrayList<SampleModel> items = new ArrayList<>();
+      for (int i = 0;i < values.size();i++) {
+        items.add(new SampleModel(values.getString(i)));
+      }
+
+      SimpleSearchDialogCompat dialog = new SimpleSearchDialogCompat(getCurrentActivity(), title,
+              searchPlaceholder, null, items,
+              new SearchResultListener<SampleModel>() {
+                @Override
+                public void onSelected(BaseSearchDialogCompat dialog,
+                                       SampleModel item, int position) {
+                  dialog.dismiss();
+                }
+              });
+
+
+      dialog.show();
+      dialog.getSearchBox().setTypeface(Typeface.SERIF);
+    }
+  }
+
+  private class SampleModel implements Searchable {
+    private String mTitle;
+
+    public SampleModel(String title) {
+      mTitle = title;
     }
 
-    alert.show(getCurrentActivity());
+    @Override
+    public String getTitle() {
+      return mTitle;
+    }
+
+    public SampleModel setTitle(String title) {
+      mTitle = title;
+      return this;
+    }
   }
 }
